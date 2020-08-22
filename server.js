16248595a0b6232e4984ffae68265de4139feb8e
kareemMorsy30 <kareemmorsy30@gmail.com>
2020-08-23 00:12:28 +0200

    Add and list users by graphql enpoints

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+var bcrypt = require('bcryptjs');
 
 // Custom modules
 const Event = require('./models/Event');
+const User = require('./models/User');
 
 const app = express();
 
 let schema = buildSchema(`
     type Query {
-        events: [Event!]!
+        events: [Event!]!,
+        users: [User!]!
     }
     type Mutation {
-        createEvent(eventInput: EventInput!):Event! 
+        createEvent(eventInput: EventInput!):Event!,
+        createUser(userInput: UserInput!):User!
     }
     input EventInput {
         title: String!,
@@ -22,12 +26,21 @@ let schema = buildSchema(`
         price: Float!,
         date: String!
     }
+    input UserInput {
+        email: String!,
+        password: String!
+    }
     type Event {
         _id: ID,
         title: String,
         description: String,
         price: Float,
         date: String
+    }
+    type User {
+        _id: ID,
+        email: String!,
+        password: String
     }
 `);
 
@@ -38,17 +51,58 @@ let events = () => {
 };
 
 let createEvent = args => {
-    let event = new Event({
-        ...args.eventInput,
-        date: new Date(args.eventInput.date)
+    return User.findById('5f41949ec52cdd3a1b5c801b')
+    .then(user => {
+        let event = new Event({
+            ...args.eventInput,
+            date: new Date(args.eventInput.date),
+            creator: user._id
+        });
+    
+        return event.save().then(result => {
+            user.createdEvents.push(result._id);
+            user.save();
+            return result;
+        });
     });
-
-    return event.save().then(result => result);
 };
+
+let users = () => {
+
+}
+
+let createUser = args => {
+    return User
+    .findOne({email: args.userInput.email})
+    .then(user => {
+        if(user){
+            throw new Error('User already exists!');
+        }else {
+            let user = new User({
+                ...args.userInput,
+                password: bcrypt.hashSync(args.userInput.password, 12)
+            })
+        
+            return user
+            .save()
+            .then(result => {
+                return {
+                    ...result._doc,
+                    password: null
+                };
+            })
+            .catch(err => {
+                throw err;
+            })
+        }
+    })
+}
 
 let rootValue = {
     events,
-    createEvent
+    createEvent,
+    users,
+    createUser
 }
 
 app.use(bodyParser.json());
